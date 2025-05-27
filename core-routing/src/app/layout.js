a990bb99b65a96eaec3269a38c96997cfd321691
subugee <sasubramanian_md@hotmail.com>
2025-05-27 10:38:40 +0530

    core routing

---
+++ core-routing/src/app/layout.js
@@ -0,0 +1,11 @@
+
+
+export default function RootLayout({ children }) {
+  return <html lang="en">
+    <body>
+      <div id="rootlayout">
+        {children}
+      </div>
+    </body>
+  </html>
+}
