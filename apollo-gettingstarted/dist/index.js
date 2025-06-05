ec55e462c1a8bb904240dec350ea2f35b15cf91c
subugee <sasubramanian_md@hotmail.com>
2025-06-05 13:23:58 +0530

    graphql schema language

---
+++ apollo-gettingstarted/dist/index.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
+//define schema
+const typeDefs = `
+    type Query {
+        hello:String
+    }
+`;
+//biz logic : resolvers
+const resolvers = {
+    Query: {
+        //api
+        hello() {
+            return "Hello Appollo Graphql!";
+        }
+    },
+    // //Mutation
+    // Mutation : {
+    // }
+};
+//create appollo server
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+});
+//deploy it on Express web Container
+const { url } = await startStandaloneServer(server, {
+    listen: {
+        port: 4000
+    }
+});
+console.log(`Apollo Server is Ready ${url}`);
